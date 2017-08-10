092e8f8752dc63f288becbcbddb91a3c16a63b6d
Artem Savkov <asavkov@redhat.com>
2017-08-10 10:16:28 +0200

    Drop GitPython dependency
    GitPython brinfs a bunch of compatability issues between versions so
    dropping it in favor of direct subprocess calls.

--- skt/__init__.py
+++ skt/__init__.py
@@ -1,4 +1,3 @@
-import git
 import logging
 import multiprocessing
 import re
@@ -10,26 +9,32 @@ import os
 class ktree(object):
     def __init__(self, uri, branch=None, wdir=None):
         self.wdir = os.path.expanduser(wdir) if wdir != None else tempfile.mkdtemp()
+        self.gdir = "%s/.git" % self.wdir
         self.uri = uri
         self.branch = branch if branch != None else "master"
         self.info = []
 
         try:
-            self.repo = git.Repo(self.wdir)
-        except (git.exc.NoSuchPathError, git.exc.InvalidGitRepositoryError):
-            self.repo = git.Repo.init(self.wdir)
+            os.mkdir(self.wdir)
+        except OSError:
+            pass
 
-        cfg = self.repo.config_reader()
-        if (not cfg.has_section('remote "origin"')):
-            self.repo.create_remote('origin', self.uri)
-        elif (self.uri != cfg.get_value('remote "origin"', 'url')):
-            self.cleanup()
-            self.repo = git.Repo.init(self.wdir)
-            self.repo.create_remote('origin', self.uri)
+        self.git_cmd("init")
+
+        try:
+            self.git_cmd("remote", "set-url", "origin", self.uri)
+        except subprocess.CalledProcessError:
+            self.git_cmd("remote", "add", "origin", self.uri)
 
         logging.info("base repo url: %s", self.uri)
         logging.info("base branch: %s", self.branch)
         logging.info("work dir: %s", self.wdir)
+
+    def git_cmd(self, *args, **kwargs):
+        args = list(["git", "--work-tree", self.wdir, "--git-dir",
+                    self.gdir]) + list(args)
+        logging.debug("executing: %s", " ".join(args))
+        subprocess.check_call(args, **kwargs)
 
     def getpath(self):
         return self.wdir
@@ -41,54 +46,87 @@ class ktree(object):
                 f.write(','.join(iitem) + "\n")
         return fpath
 
-    def checkout(self):
-        logging.info("fetching base repo")
-        self.repo.remote().fetch()
+    def get_head(self, ref):
+        head = None
+        with open(os.path.join(self.gdir, ref), 'r') as f:
+            head = f.readline()
 
-        if not 'master' in self.repo.heads:
-            self.repo.create_head('master',
-                    self.repo.remote().refs[self.branch])
+        return head
+
+    def checkout(self):
+        dstref = "refs/remotes/origin/%s" % (self.branch)
+        logging.info("fetching base repo")
+        self.git_cmd("fetch", "-n", "origin",
+                     "+refs/heads/%s:%s" %
+                      (self.branch, dstref))
 
         logging.info("checking out %s branch", self.branch)
-        self.repo.heads.master.checkout()
-        self.repo.head.reset("origin/%s" % self.branch,
-                             index = True,
-                             working_tree = True)
+        self.git_cmd("reset", "--hard", dstref)
 
-        self.info.append(("git", self.uri, str(self.repo.head.commit)))
-        logging.info("baserepo %s: %s", self.branch, self.repo.head.commit)
+        head = self.get_head(dstref)
+        self.info.append(("git", self.uri, head))
+        logging.info("baserepo %s: %s", self.branch, head)
 
     def cleanup(self):
         logging.info("cleaning up %s", self.wdir)
         shutil.rmtree(self.wdir)
 
+    def get_remote_url(self, remote):
+        rurl = None
+        try:
+            grs = subprocess.Popen(["git",
+                                    "--work-tree", self.wdir,
+                                    "--git-dir", self.gdir,
+                                    "remote", "show", remote],
+                                   stdout = subprocess.PIPE,
+                                   stderr = subprocess.PIPE)
+            (stdout, stderr) = grs.communicate()
+            for line in stdout.split("\n"):
+                m = re.match('Fetch URL: (.*)', line)
+                if m:
+                    rurl = m.group(1)
+                    break
+        except subprocess.CalledProcessError:
+            pass
+
+        return rurl
+
+
     def getrname(self, uri):
         rname = uri.split('/')[-1].replace('.git', '') if not uri.endswith('/') else uri.split('/')[-2].replace('.git', '')
-        cfg = self.repo.config_reader()
-        while cfg.has_section('remote "%s"' % rname) and (uri !=
-                cfg.get_value('remote "%s"' % rname, 'url')):
-            print "Already exists '%s', adding _" % rname
+        while self.get_remote_url(rname) == uri:
+            logging.warning("remote '%s' already exists with a different uri, adding '_'" % rname)
             rname += '_'
 
         return rname
 
     def merge_git_branch(self, uri, branch="master"):
         rname = self.getrname(uri)
+
         try:
-            remote = self.repo.remote(rname)
-        except ValueError:
-            remote = self.repo.create_remote(rname, uri)
+            self.git_cmd("remote", "add", rname, uri, stderr = subprocess.PIPE)
+        except subprocess.CalledProcessError:
+            pass
+
+        dstref = "refs/remotes/%s/%s" % (rname, branch)
+        logging.info("fetching %s", dstref)
+        self.git_cmd("fetch", "-n", rname,
+                     "+refs/heads/%s:%s" %
+                      (branch, dstref))
 
         logging.info("merging %s: %s", rname, branch)
         try:
-            remote.pull(branch)
-            self.info.append(("git", uri, str(remote.refs[branch].commit)))
-            logging.info("%s %s: %s", rname, branch,
-                         remote.refs[branch].commit)
-        except git.exc.GitCommandError:
+            grargs = { 'stdout' : subprocess.PIPE } if \
+                logging.getLogger().level > logging.DEBUG else {}
+
+            self.git_cmd("merge", "--no-edit", dstref, **grargs)
+            head = self.get_head(dstref)
+            self.info.append(("git", uri, head))
+            logging.info("%s %s: %s", rname, branch, head)
+        except subprocess.CalledProcessError:
             logging.warning("failed to merge '%s' from %s, skipping", branch,
                             rname)
-            self.repo.head.reset(index = True, working_tree = True)
+            self.git_cmd("reset", "--hard")
 
     def merge_patchwork_patch(self, uri):
         pass
