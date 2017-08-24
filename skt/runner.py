28a627a0ff1648f2947e3c3bfd5d954df4f260df
Artem Savkov <asavkov@redhat.com>
2017-08-24 17:52:48 +0200

    runner/beaker: automatic job resubmission
    On any failure resubmit the job twice: once on a random host, once on
    the same host.

--- skt/runner.py
+++ skt/runner.py
@@ -16,6 +16,7 @@ class beakerrunner(runner):
         self.jobowner = jobowner
         self.watchdelay = 60
         self.watchlist = set()
+        self.whiteboard = None
 
         logging.info("runner type: %s", self.TYPE)
         logging.info("beaker template: %s", self.template)
@@ -52,42 +53,89 @@ class beakerrunner(runner):
 
         return ret
 
+    def recipe_to_job(self, recipe, samehost = False):
+        tmp = recipe.copy()
+        if (samehost):
+            hreq = tmp.find("hostRequires")
+            hostname = etree.Element("hostname")
+            hostname.set("op", "=")
+            hostname.set("value", tmp.attrib.get("system"))
+            hreq.append(hostname)
+
+        newrs = etree.Element("recipeSet")
+        newrs.append(tmp)
+
+        newwb = etree.Element("whiteboard")
+        newwb.text = "%s [R:%s]" % (self.whiteboard, tmp.attrib.get("id"))
+
+        if (samehost):
+            newwb.text += " (%s)" % tmp.attrib.get("system")
+
+        newroot = etree.Element("job")
+        newroot.append(newwb)
+        newroot.append(newrs)
+
+        return newroot
+
     def watchloop(self):
+        iteration = 0
         while len(self.watchlist):
-            time.sleep(self.watchdelay)
-            for cid in self.watchlist.copy():
+            if iteration > 0:
+                time.sleep(self.watchdelay)
+
+            for (cid, reschedule) in self.watchlist.copy():
                 bkr = subprocess.Popen(["bkr", "job-results", "--no-logs",
                                         cid],
                                        stdout=subprocess.PIPE)
                 (stdout, stderr) = bkr.communicate()
                 root = etree.fromstring(stdout)
 
-                logging.debug("status %s: %s (%s)", cid,
+                logging.debug("[%d] status %s: %s (%s)", iteration, cid,
                               root.attrib.get("status"),
                               root.attrib.get("result"))
-                if root.attrib.get("status") in ["Completed", "Aborted"]:
+
+                if root.attrib.get("status") in ["Completed", "Aborted",
+                                                 "Cancelled"]:
                     logging.info("%s status changed to '%s', removing from watchlist",
                                  cid, root.attrib.get("status"))
-                    self.watchlist.remove(cid)
+                    self.watchlist.remove((cid, reschedule))
 
-    def wait(self, jobid):
+                    if root.attrib.get("status") ==  "Cancelled":
+                        continue
+
+                    if reschedule and root.attrib.get("result") != "Pass":
+                        logging.info("%s -> '%s', resubmitting",
+                                     cid, root.attrib.get("result"))
+
+                        newjob = self.recipe_to_job(root, False)
+                        newjobid = self.jobsubmit(etree.tostring(newjob))
+                        self.add_to_watchlist(newjobid, False)
+
+                        newjob = self.recipe_to_job(root, True)
+                        newjobid = self.jobsubmit(etree.tostring(newjob))
+                        self.add_to_watchlist(newjobid, False)
+
+            iteration += 1
+
+    def add_to_watchlist(self, jobid, reschedule=True):
         bkr = subprocess.Popen(["bkr", "job-results", "--no-logs", jobid],
                                stdout=subprocess.PIPE)
         (stdout, stderr) = bkr.communicate()
         root = etree.fromstring(stdout)
 
-        if root.attrib.get("status") in ["Completed", "Aborted"]:
-            return
+        if self.whiteboard == None:
+            self.whiteboard = root.find("whiteboard").text
 
         for el in root.findall("recipeSet/recipe"):
             cid = "R:%s" % el.attrib.get("id")
-            self.watchlist.add(cid)
+            self.watchlist.add((cid,reschedule))
             logging.info("added %s to watchlist", cid)
 
+    def wait(self, jobid):
+        self.add_to_watchlist(jobid, True)
         self.watchloop()
 
-    def run(self, url, release, wait=False):
-        ret = 0
+    def jobsubmit(self, xml):
         jobid = None
         args = ["bkr", "job-submit"]
 
@@ -96,13 +144,10 @@ class beakerrunner(runner):
 
         args += ["-"]
 
-        uid = url.split('/')[-1]
-
         bkr = subprocess.Popen(args, stdin=subprocess.PIPE,
                                stdout=subprocess.PIPE)
-        (stdout, stderr) = bkr.communicate(self.getxml({'KVER' : release,
-                                                        'KPKG_URL' : url,
-                                                        'UID': uid}))
+
+        (stdout, stderr) = bkr.communicate(xml)
 
         for line in stdout.split("\n"):
             m = re.match("^Submitted: \['([^']+)'\]$", line)
@@ -110,7 +155,16 @@ class beakerrunner(runner):
                 jobid = m.group(1)
                 break
 
-        logging.info("jobid: %s", jobid)
+        return jobid
+
+    def run(self, url, release, wait=False):
+        ret = 0
+        uid = url.split('/')[-1]
+        jobid = self.jobsubmit(self.getxml({'KVER' : release,
+                                            'KPKG_URL' : url,
+                                            'UID': uid}))
+
+        logging.info("main jobid: %s", jobid)
 
         if wait == True:
             self.wait(jobid)
