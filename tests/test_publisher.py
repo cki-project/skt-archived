3e99d791bcbab2697882b8036f3c3c37e9f4dbee
Agustin Henze <tin@aayy.com.ar>
2018-03-06 18:09:33 -0300

    Document tests/test_publisher.py code

--- tests/test_publisher.py
+++ tests/test_publisher.py
@@ -1,8 +1,13 @@
+"""
+Test cases for publisher module.
+"""
 import unittest
 from skt import publisher
 
 
 class TestPublisher(unittest.TestCase):
+    """Test cases for publisher.publisher class"""
     def test_geturl(self):
+        """Check if the source url is built correctly"""
         pub = publisher.publisher('dest', 'file:///tmp/test')
         self.assertEqual(pub.geturl('source'), 'file:///tmp/test/source')
