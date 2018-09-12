e2caa49149eb6e3ad36f59fd7043107cc9159911
danghai <haihoangdang91@gmail.com>
2018-09-12 00:38:49 -0700

    Add the module docstrings

--- skt/reporter.py
+++ skt/reporter.py
@@ -11,6 +11,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+"""Class for managing Reporter."""
 import ConfigParser
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
