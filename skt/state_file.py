742639104459369ce375c9ffac544cc8fcada4c9
Major Hayden <major@redhat.com>
2019-01-16 11:43:37 -0600

    Remove unused state_file functions/tests
    Signed-off-by: Major Hayden <major@redhat.com>

--- skt/state_file.py
+++ skt/state_file.py
@@ -13,77 +13,7 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 """Functions that manage the skt state file."""
 import ConfigParser
-import logging
 import os
-import yaml
-
-
-def destroy(cfg):
-    """
-    Destroy the state file.
-
-    Args:
-        cfg: A dictionary of skt configuration
-    """
-    state_file = cfg.get('state')
-
-    if os.path.isfile(state_file):
-        try:
-            os.unlink(state_file)
-        except IOError as exception:
-            logging.error("Failed to delete state file: %s", exception)
-            raise exception
-
-
-def read(cfg):
-    """
-    Read the state file from the disk.
-
-    Args:
-        cfg: A dictionary of skt configuration
-    """
-    # Return an empty state dictionary if the state file does not exist
-    current_state = {}
-
-    state_file = cfg.get('state')
-    logging.debug("Reading state from: %s", state_file)
-
-    if os.path.isfile(state_file):
-        try:
-            with open(state_file, 'r') as fileh:
-                current_state = yaml.load(fileh)
-        except IOError as exception:
-            logging.error("Failed to read state file: %s", exception)
-            raise exception
-
-    return current_state
-
-
-def update(cfg, state_updates):
-    """
-    Update the state file on disk with new state data.
-
-    Args:
-        cfg:           A dictionary of skt configuration.
-        state_updates: A dictionary of items to update.
-    """
-    current_state = read(cfg)
-
-    # Merge the current state and the updates.
-    new_state = current_state.copy()
-    new_state.update(state_updates)
-
-    # Save the new state to the state file
-    logging.debug("Saving state: %s", state_updates)
-    state_file = cfg.get('state')
-
-    try:
-        with open(state_file, 'w') as fileh:
-            yaml_state = yaml.dump(new_state, default_flow_style=False)
-            fileh.write(yaml_state)
-    except IOError as exception:
-        logging.error("Failed to update state file: %s", exception)
-        raise exception
 
 
 def update_state(state_file, state_dict):
