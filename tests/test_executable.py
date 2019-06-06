e1b52b9df1ebf5e7da5f7a5d5b7c599119631211
Jakub Racek <jracek@redhat.com>
2019-06-06 16:51:52 +0200

    drop unused code
    Signed-off-by: Jakub Racek <jracek@redhat.com>

--- tests/test_executable.py
+++ tests/test_executable.py
@@ -14,45 +14,13 @@
 """Test cases for runner module."""
 import logging
 import os
-import sys
 import unittest
-from io import StringIO
 
 from skt import executable
 
 
 class TestExecutable(unittest.TestCase):
     """Test cases for executable module."""
-
-    # pylint: disable=too-many-public-methods
-    def check_args_tester(self, args, expected_fail=True,
-                          expected_stdout=None, expected_stderr=None):
-        """Reusable method to test the check_args() method."""
-        parser = executable.setup_parser()
-
-        # Capture stdout/stderr temporarily
-        old_stdout = sys.stdout
-        old_stderr = sys.stderr
-        temp_stdout = StringIO()
-        temp_stderr = StringIO()
-        sys.stdout = temp_stdout
-        sys.stderr = temp_stderr
-
-        if expected_fail:
-            with self.assertRaises(SystemExit):
-                args = parser.parse_args(args)
-        else:
-            args = parser.parse_args(args)
-
-        if expected_stdout:
-            self.assertIn(expected_stdout, temp_stdout.getvalue().strip())
-
-        if expected_stderr:
-            self.assertIn(expected_stderr, temp_stderr.getvalue().strip())
-
-        # Reset stdout/stderr
-        sys.stdout = old_stdout
-        sys.stderr = old_stderr
 
     def test_full_path_relative(self):
         """Verify that full_path() expands a relative path."""
