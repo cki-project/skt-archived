14b605b44ecbf76514d1ee6ee6a670cbf38d898d
Jakub Racek <jracek@redhat.com>
2020-01-21 17:24:18 +0100

    add docstring and bump license year
    Signed-off-by: Jakub Racek <jracek@redhat.com>

--- skt/executable.py
+++ skt/executable.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2017-2019 Red Hat, Inc. All rights reserved. This copyrighted
+# Copyright (c) 2017-2020 Red Hat, Inc. All rights reserved. This copyrighted
 # material is made available to anyone wishing to use, modify, copy, or
 # redistribute it subject to the terms and conditions of the GNU General
 # Public License v.2 or later.
@@ -11,7 +11,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-
+"""SKT entry point and argument parsing."""
 import argparse
 import atexit
 import itertools
